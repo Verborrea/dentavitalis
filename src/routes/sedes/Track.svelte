edad7b1880248cae9cf37dead23ffdcdcb4f8fa8
Alan Monroy Bernedo <alan.monroy@ucsp.edu.pe>
2023-09-09 16:24:50 -0500

    Stage 1: Sedes

---
+++ src/routes/sedes/Track.svelte
@@ -0,0 +1,26 @@
+<div class="image-track">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+    <img draggable="false" class="image" src="https://gacetadental.com/wp-content/uploads/2022/07/abrir-clinica-dental-documentacion.jpg" alt="img">
+</div>
+
+
+<style>
+    .image {
+        width: 40vmin;
+        height: 56vmin;
+        object-fit: cover;
+        object-position: center;
+    }
+    .image-track {
+        display: flex;
+        gap: 4vmin;
+        position: relative;
+        left: 50%;
+    }
+</style>
